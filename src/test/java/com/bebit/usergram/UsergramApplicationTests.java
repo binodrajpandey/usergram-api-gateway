9a0bd9da8505853d47005238a525f67b88896ddb
Binod <binodbex@gmail.com>
2024-09-19 20:15:23 +0900

    Initialize project

---
+++ src/test/java/com/bebit/usergram/UsergramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bebit.usergram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsergramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
